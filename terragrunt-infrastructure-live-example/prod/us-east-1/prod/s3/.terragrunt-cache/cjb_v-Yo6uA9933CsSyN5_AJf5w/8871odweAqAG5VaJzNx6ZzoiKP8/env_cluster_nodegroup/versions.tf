0882a982631bbd61d85bddbb44b5a260471e8181
Surya Oruganti <suryaoruganti@gmail.com>
2021-07-20 16:46:45 -0700

    remove temp files

--- terragrunt-infrastructure-live-example/prod/us-east-1/prod/s3/.terragrunt-cache/cjb_v-Yo6uA9933CsSyN5_AJf5w/8871odweAqAG5VaJzNx6ZzoiKP8/env_cluster_nodegroup/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.15"
-
-  required_providers {
-    aws        = ">= 3.40.0"
-    local      = ">= 1.4"
-    kubernetes = ">= 1.20"
-  }
-}
--- terragrunt-infrastructure-live-example/prod/us-east-1/prod/webserver-cluster/.terragrunt-cache/IjHk4GUgQiHFB46oM9Tx6-PA2iU/aaziAt7NWFUv5FSF1QTpkG6FxVw/aws/env_cluster_nodegroup/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "pg" {
-    conn_str = "postgres://{{.BackendData.Username}}:{{.BackendData.Password}}@{{.BackendData.Host}}/{{.Organization.Name}}"
-    schema_name = "tf_{{.Environment.Name}}"
-  }
-}
