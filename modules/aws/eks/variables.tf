1935d94f334d78b6b24e2707fe70ec40ad579cbe
Surya Oruganti <suryaoruganti@gmail.com>
2021-08-29 15:49:58 -0700

    eks nodegroup naming convention change

--- modules/aws/eks/variables.tf
+++ modules/aws/eks/variables.tf
@@ -20,11 +20,6 @@ variable "on_demand_tags" {
 variable "map_accounts" {
   description = "Additional AWS account numbers to add to the aws-auth configmap."
   type        = list(string)
-
-  # default = [
-  #   "777777777777",
-  #   "888888888888",
-  # ]
 }
 
 variable "cluster" {
@@ -61,33 +56,6 @@ variable "node_group" {
   })
 }
 
-# variable "node_group_tags" {
-#   description = "All the node group tags apart from the default ones for all the resources"
-#   type = map(string)
-# }
-
-# variable "node_group_labels" {
-#   description = "All the node group labels"
-#   type = map(string)
-# }
-
-# variable "map_roles" {
-#   description = "Additional IAM roles to add to the aws-auth configmap."
-#   type = list(object({
-#     rolearn  = string
-#     username = string
-#     groups   = list(string)
-#   }))
-
-#   # default = [
-#   #   {
-#   #     rolearn  = "arn:aws:iam::66666666666:role/role1"
-#   #     username = "role1"
-#   #     groups   = ["system:masters"]
-#   #   },
-#   # ]
-# }
-
 variable "map_users" {
   description = "Additional IAM users to add to the aws-auth configmap."
   type = list(object({
@@ -96,18 +64,6 @@ variable "map_users" {
     groups   = list(string)
   }))
 
-  # default = [
-  #   {
-  #     userarn  = "arn:aws:iam::66666666666:user/user1"
-  #     username = "user1"
-  #     groups   = ["system:masters"]
-  #   },
-  #   {
-  #     userarn  = "arn:aws:iam::66666666666:user/user2"
-  #     username = "user2"
-  #     groups   = ["system:masters"]
-  #   },
-  # ]
 }
 
 variable "env" {
