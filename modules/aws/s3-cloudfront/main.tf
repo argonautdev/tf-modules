58f0e39fb17089db962b63c0ca85971e325d0c10
Surya Oruganti <suryaoruganti@gmail.com>
2021-10-06 12:42:51 -0700

    reintro lambda parts

--- modules/aws/s3-cloudfront/main.tf
+++ modules/aws/s3-cloudfront/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 locals {
-  domain_name = "previewapp.live" # trimsuffix(data.aws_route53_zone.this.name, ".")
+  domain_name = "argonaut.live" # trimsuffix(data.aws_route53_zone.this.name, ".")
   subdomain   = "cdn"
   app_name    = "myapp"
 }
