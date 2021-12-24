83848fa0f7934bbcd010f60417fa75b6fe59d3db
PrashantRaj18198 <prashant@argonaut.dev>
2021-12-24 15:04:34 +0530

    feat: add associated security groups

--- modules/aws/msk/main.tf
+++ modules/aws/msk/main.tf
@@ -24,10 +24,11 @@ module "msk" {
   version = "0.8.2"
 
   zone_id                = var.zone_id
-  security_groups        = [module.security_group.security_group_id]
+  security_groups        = [module.security_group.security_group_id, var.vpc.default_security_group_id]
   vpc_id                 = var.vpc.vpc_id
   subnet_ids             = var.vpc.private_subnets
   allowed_cidr_blocks    = concat([var.vpc.vpc_cidr_block], var.allowed_cidr_blocks)
+  associated_security_group_ids = [module.security_group.security_group_id, var.vpc.default_security_group_id]
   kafka_version          = var.kafka_version
   number_of_broker_nodes = var.number_of_broker_nodes
   broker_instance_type   = var.broker_instance_type
