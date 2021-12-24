8280bf88adef4d6a069a79bdb162c992a58cb89c
PrashantRaj18198 <prashant@argonaut.dev>
2021-12-24 13:27:57 +0530

    fix: wrong field reference

--- modules/aws/msk/main.tf
+++ modules/aws/msk/main.tf
@@ -5,7 +5,7 @@ module "msk" {
   zone_id                = var.zone_id
   security_groups        = [var.vpc.default_security_group_id]
   vpc_id                 = module.vpc.vpc_id
-  subnet_ids             = var.vpc.private_subnet_ids
+  subnet_ids             = var.vpc.private_subnets
   kafka_version          = var.kafka_version
   number_of_broker_nodes = var.number_of_broker_nodes
   broker_instance_type   = var.broker_instance_type
