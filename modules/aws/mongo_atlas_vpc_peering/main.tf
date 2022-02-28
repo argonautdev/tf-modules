8bd159372c5a0a03716a34397adda9f92d57af67
Sumit <sumit@argonaut.dev>
2022-02-28 20:13:25 +0530

    Removed resource which creates mongodb atlas network peering container Added atlas_container_id field to accept existing container id from terragrunt

--- modules/aws/mongo_atlas_vpc_peering/main.tf
+++ modules/aws/mongo_atlas_vpc_peering/main.tf
@@ -8,22 +8,15 @@ data "mongodbatlas_project" "aws_atlas" {
   project_id = var.atlas_project_id
 }
 
-resource "mongodbatlas_network_container" "atlas_container" {
-  atlas_cidr_block = var.atlas_vpc_cidr
-  project_id       = data.mongodbatlas_project.aws_atlas.id
-  provider_name    = "AWS"
-  region_name      = var.atlas_region
-}
-
 data "mongodbatlas_network_container" "atlas_container" {
-  container_id = mongodbatlas_network_container.atlas_container.container_id
+  container_id = var.atlas_container_id
   project_id   = data.mongodbatlas_project.aws_atlas.id
 }
 
 resource "mongodbatlas_network_peering" "aws-atlas" {
   accepter_region_name   = var.aws_region
   project_id             = data.mongodbatlas_project.aws_atlas.id
-  container_id           = mongodbatlas_network_container.atlas_container.container_id
+  container_id           = var.atlas_container_id
   provider_name          = "AWS"
   route_table_cidr_block = data.aws_vpc.primary.cidr_block
   vpc_id                 = data.aws_vpc.primary.id
--- modules/aws/mongo_atlas_vpc_peering/variables.tf
+++ modules/aws/mongo_atlas_vpc_peering/variables.tf
@@ -5,34 +5,39 @@ variable "aws_region" {
 
 variable "vpc" {
   description = "All vpc info"
-  type = object({
-    name = string
-    vpc_id   = string
+  type        = object({
+    name   = string
+    vpc_id = string
   })
 }
 
 variable "atlas_public_key" {
-  type = string
+  type        = string
   description = "The public API key for MongoDB Atlas"
 }
 
 variable "atlas_private_key" {
-  type = string
+  type        = string
   description = "The private API key for MongoDB Atlas"
 }
 
 variable "atlas_region" {
-  type = string
+  type        = string
   description = "Atlas Region"
 }
 
 variable "atlas_vpc_cidr" {
   description = "Atlas CIDR"
-  type = string
+  type        = string
 }
 
 variable "atlas_project_id" {
   description = "MongoDB Atlas project id"
+  type        = string
+}
+
+variable "atlas_container_id" {
+  description = "MongoDB Atlas network container id"
   type        = string
 }
 
