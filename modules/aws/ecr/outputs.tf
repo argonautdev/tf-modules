dcbf3442ed38e5f14b338469afb9724975b9322c
pk0331 <ec2-user@ip-172-31-47-163.ap-south-1.compute.internal>
2022-07-20 01:59:32 +0000

    Removing aws module

--- modules/aws/ecr/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repositories" {
-  value = module.ecr
-}
-output "ecr_replication" {
-  value = aws_ecr_replication_configuration.default
-}
-
--- modules/aws/eks/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-  default_tags {
-    tags = var.default_tags
-  }
-}
