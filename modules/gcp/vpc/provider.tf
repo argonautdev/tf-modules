59cda24eafb6fea93b8d7943adcd7a22015d6036
pk0331 <ec2-user@ip-172-31-47-163.ap-south-1.compute.internal>
2022-08-04 03:07:41 +0000

    GCP GKE, VPC module changes

--- modules/gcp/vpc/provider.tf
+++ modules/gcp/vpc/provider.tf
@@ -13,7 +13,6 @@ terraform {
 ##Default Tag limited to AWS Provider which is not availble in GCP. Hence commented out.
 ##Region Codes: https://cloud.google.com/about/locations#americas
 provider "google" {
-  # credentials = "${file("${path.module}/../svc-account.json")}"
   project = var.project_id
-  # region  = var.region
+  region  = var.region
 }
