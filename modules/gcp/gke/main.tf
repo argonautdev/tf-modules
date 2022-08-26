14bf34ea43415596053565c041b2012588842c8d
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2022-08-26 10:55:33 +0000

    adding delay before creating resources

--- modules/gcp/gke/main.tf
+++ modules/gcp/gke/main.tf
@@ -19,7 +19,7 @@ resource "null_resource" "previous" {
 }
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [module.enabled_google_apis]
+  depends_on = [module.enabled_google_apis, null_resource.previous]
   create_duration = "30s"
 }
 
