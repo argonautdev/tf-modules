2c002f9c75253fa0c22cc3f3f98b926e94bf4b61
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2022-09-22 16:07:23 +0000

    Memorystore for redis

---
+++ modules/gcp/memorystore-redis/variables.tf
@@ -0,0 +1,158 @@
+variable "project_id" {
+  description = "The project ID to manage the Cloud SQL resources"
+  type        = string
+}
+
+// required
+variable "region" {
+  description = "The region of the Cloud SQL resources"
+  type        = string
+  default     = "us-east4"
+}
+
+variable "name" {
+  description = "The ID of the instance or a fully qualified identifier for the instance."
+  type        = string
+}
+
+variable "vpc_network_name" {
+  description = "The VPC Network name"
+  type        = string
+}
+
+## Comparision between tiers ( https://cloud.google.com/memorystore/docs/redis/redis-tiers )
+variable "tier" {
+  description = "The service tier of the instance. https://cloud.google.com/memorystore/docs/redis/reference/rest/v1/projects.locations.instances#Tier"
+  type        = string
+  default     = "STANDARD_HA"
+  
+  validation {
+    condition     = var.tier == "BASIC" || var.tier == "STANDARD_HA" 
+    error_message = "The value choosen is not in the list of ( BASIC, STANDARD_HA )."
+  }
+}
+
+##Maximum range Size in GB is 300
+variable "memory_size_gb" {
+  description = "Redis memory size in GiB. Defaulted to 1 GiB"
+  type        = number
+  default     = 1
+}
+
+##Valid only when replica enabled
+variable "replica_count" {
+  description = "The number of replicas. Valid only when 'READ_REPLICAS_DISABLED' Disabled"
+  type        = number
+  default     = null
+}
+
+variable "read_replicas_mode" {
+  description = "Read replicas mode. https://cloud.google.com/memorystore/docs/redis/reference/rest/v1/projects.locations.instances#readreplicasmode "
+  type        = string
+  default     = "READ_REPLICAS_DISABLED"
+  
+  validation {
+    condition     = var.read_replicas_mode == "READ_REPLICAS_DISABLED" || var.read_replicas_mode == "READ_REPLICAS_ENABLED"
+    error_message = "The value choosen is not in the list of ( BASIC, STANDARD_HA )."
+  }
+}
+
+variable "location_id" {
+  description = "The zone where the instance will be provisioned. If not provided, the service will choose a zone for the instance. For STANDARD_HA tier, instances will be created across two zones for protection against zonal failures. If [alternativeLocationId] is also provided, it must be different from [locationId]."
+  type        = string
+  default     = null
+}
+
+variable "alternative_location_id" {
+  description = "The alternative zone where the instance will be provisioned. a.k.a ( secondary availability zone )"
+  type        = string
+  default     = null
+}
+
+variable "redis_version" {
+  description = "The version of Redis software."
+  type        = string
+  default     = "REDIS_6_X"
+  
+  validation {
+    condition     = var.redis_version == "REDIS_3_2" || var.redis_version == "REDIS_4_0" || var.redis_version == "REDIS_5_0" || var.redis_version == "REDIS_6_X"
+    error_message = "The value choosen is not in the list of ( REDIS_3_2, REDIS_4_0,  REDIS_5_0, REDIS_6_X)."
+  }
+}
+
+variable "redis_configs" {
+  description = "The Redis configuration parameters. See [more details](https://cloud.google.com/memorystore/docs/redis/reference/rest/v1/projects.locations.instances#Instance.FIELDS.redis_configs)"
+  type        = map(any)
+  default     = {}
+}
+
+variable "display_name" {
+  description = "An arbitrary and optional user-provided name for the instance."
+  type        = string
+  default     = null
+}
+
+variable "reserved_ip_range" {
+  description = "The CIDR range of internal addresses that are reserved for this instance."
+  type        = string
+  default     = null
+}
+
+## Difference Between DirectPeering and Private Service access is seen here (https://cloud.google.com/memorystore/docs/redis/networking)
+
+variable "connect_mode" {
+  description = "The connection mode of the Redis instance. Can be either DIRECT_PEERING or PRIVATE_SERVICE_ACCESS. The default connect mode if not provided is DIRECT_PEERING."
+  type        = string
+  default     = "PRIVATE_SERVICE_ACCESS"
+  
+  validation {
+    condition     = var.connect_mode == "DIRECT_PEERING" || var.connect_mode == "PRIVATE_SERVICE_ACCESS"
+    error_message = "The value choosen is not in the list of ( DIRECT_PEERING, PRIVATE_SERVICE_ACCESS)."
+  }
+  
+}
+
+variable "labels" {
+  description = "The resource labels to represent user provided metadata."
+  type        = map(string)
+  default     = null
+}
+
+variable "default_labels" {
+  type        = map(string)
+  default     = {}
+  description = "The key/value labels for the master instances."
+}
+
+variable "auth_enabled" {
+  description = "Indicates whether OSS Redis AUTH is enabled for the instance. If set to true AUTH is enabled on the instance."
+  type        = bool
+  default     = false
+}
+
+variable "transit_encryption_mode" {
+  description = "The TLS mode of the Redis instance, If not provided, TLS is enabled for the instance."
+  type        = string
+  default     = "SERVER_AUTHENTICATION"
+}
+
+variable "maintenance_policy" {
+  description = "The maintenance policy for an instance."
+  # type = object(any)
+  type = object({
+    day = string
+    start_time = object({
+      hours   = number
+      minutes = number
+      seconds = number
+      nanos   = number
+    })
+  })
+  default = null
+}
+
+variable "customer_managed_key" {
+  description = "Default encryption key to apply to the Redis instance. Defaults to null (Google-managed)."
+  type        = string
+  default     = null
+}
