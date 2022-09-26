6c52e99763d6d975108d99186f7cd3851a96e6ef
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2022-09-26 05:26:12 +0000

    Adding EnableAPI module block for enabling memcached api, which doesn't disable on terraform destroy

--- modules/gcp/memorystore-memcache/main.tf
+++ modules/gcp/memorystore-memcache/main.tf
@@ -1,9 +1,25 @@
+module "enable_apis" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 11.0"
+
+  project_id  = var.project_id
+  enable_apis = true
+  disable_services_on_destroy = false
+  
+  activate_apis = [
+    "memcache.googleapis.com",
+  ]
+}
+
+
 module "memcache" {
+  depends_on         = [module.enable_apis]
   source  = "terraform-google-modules/memorystore/google//modules/memcache"
   version = "4.4.1"
   region = var.region
   project = var.project_id
   name    = var.name
+  enable_apis = var.enable_apis
   authorized_network = "projects/${var.project_id}/global/networks/${var.vpc_network_name}"
   node_count = var.node_count
   cpu_count = var.cpu_count
