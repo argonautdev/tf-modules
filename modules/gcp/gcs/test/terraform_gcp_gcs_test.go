b940ab3d63e72631a39836ce2f64329d225d055c
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2022-10-25 16:34:07 +0000

    GCS Testing using Terratest

--- modules/gcp/gcs/test/terraform_gcp_gcs_test.go
+++ modules/gcp/gcs/test/terraform_gcp_gcs_test.go
@@ -4,7 +4,6 @@ import (
 	"cloud.google.com/go/storage"
 	"context"
 	"fmt"
-	"github.com/gruntwork-io/terratest/modules/gcp"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -13,7 +12,8 @@ import (
 	"testing"
 )
 
-func getBucketMetadata(t *testing.T, bucketname string) {
+// https://cloud.google.com/storage/docs/getting-bucket-information#storage-get-bucket-metadata-go ---> Reference for following funciton
+func getBucketMetadata(t *testing.T, bucketname string) (*storage.BucketAttrs, error) {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -23,8 +23,9 @@ func getBucketMetadata(t *testing.T, bucketname string) {
 	attrs, err := client.Bucket(bucketname).Attrs(ctx)
 	if err != nil {
 		t.Fatal(err)
+		t.Log("error while retriving bucket attributes")
 	}
-	return attrs
+	return attrs, err
 }
 
 // An example of how to test the Terraform module in modules/aws/rds using Terratest.
@@ -37,11 +38,10 @@ func TestTerraformModule(t *testing.T) {
 	bucket_access_level := "private"
 	prefix := "terratest"
 	project_id := "playground-351903"
-	default_tags := "{ \"argonaut.dev/name\":  \"mariadb-test\", \"argonaut.dev/type\": \"GCS\", \"argonaut.dev/manager\":  \"argonaut.dev\", \"argonaut.dev/GCS\": \"region\", \"argonaut.dev/env/dev\":  \"true\"}"
+	//default_labels := "{ \"argonaut.dev/name\":  \"gcs-test\", \"argonaut.dev/type\": \"GCS\", \"argonaut.dev/manager\":  \"argonaut.dev\", \"argonaut.dev/GCS\": \"region\", \"argonaut.dev/env/dev\":  \"true\"}"
 
 	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
 	// terraform testing.
-
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// The path to where our Terraform code is located
 		// TerraformDir: "../modules/gcp/gcs",
@@ -50,12 +50,14 @@ func TestTerraformModule(t *testing.T) {
 		// Variables to pass to our Terraform code using -var options
 		// "username" and "password" should not be passed from here in a production scenario.
 		Vars: map[string]interface{}{
-			"project_id":    project_id,
-			"region":        region,
-			"names":         "[\"bucketname\"]",
-			"prefix":        prefix,
-			"storage_class": storage_class,
-			"default_tags":  default_tags,
+			"project_id":          project_id,
+			"region":              region,
+			"names":               fmt.Sprintf("[\"%s\"]", bucketname),
+			"prefix":              prefix,
+			"storage_class":       storage_class,
+			"bucket_access_level": bucket_access_level,
+			//"default_labels":      default_labels,
+			"location": region,
 		},
 		Lock: false,
 	})
@@ -66,11 +68,17 @@ func TestTerraformModule(t *testing.T) {
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
-	getbucketmetadata = getBucketMetadata(t, bucketname)
+	getbucketmetadata, err := getBucketMetadata(t, fmt.Sprintf("%s-%s-%s", prefix, region, bucketname))
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	//Applied StorageClass should be "STANDARD"
 	assert.Equal(t, "STANDARD", getbucketmetadata.StorageClass)
 
 	//Applied BucketACL should be Private
-	assert.Equal(t, "private", getbucketmetadata.PredefinedACL)
+	assert.Equal(t, strings.ToUpper(region), getbucketmetadata.Location)
+
+	t.Log("Checking Versioning Status...")
+	assert.Equal(t, false, getbucketmetadata.VersioningEnabled)
 }
