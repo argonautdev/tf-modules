ef102d967325b9a84c049e4f64627509ea943ddc
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2022-11-03 03:08:21 +0000

    name prefix change

--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -29,7 +29,8 @@ locals {
   ng_list_replacements = { for k, v in local.ng_list : k => merge({
     capacity_type = v.spot ? "SPOT" : "ON_DEMAND"
     instance_types = [v.instance_type]
-    name_prefix   = "${v.ng_name}-art-"
+    # name_prefix   = "${v.ng_name}-art-"
+    name_prefix = "${var.cluster.name}-art-"
     k8s_labels = merge(v.k8s_labels, { Environment = var.env })
   }, v)}
 }
