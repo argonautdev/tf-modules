e908e49d479ce8a20b9ee4e779f3ff8296ea9050
pk0331 <105767668+pk0331@users.noreply.github.com>
2023-03-29 09:45:17 +0530

    Mongo Atlas module changes (#104)

--- modules/aws/mongo_atlas_vpc_peering/main.tf
+++ modules/aws/mongo_atlas_vpc_peering/main.tf
@@ -8,6 +8,7 @@ data "mongodbatlas_project" "aws_atlas" {
   project_id = var.atlas_project_id
 }
 
+##Container is Just VPC
 resource "mongodbatlas_network_container" "atlas_container" {
   atlas_cidr_block = var.atlas_vpc_cidr
   project_id       = data.mongodbatlas_project.aws_atlas.id
@@ -30,11 +31,54 @@ resource "mongodbatlas_network_peering" "aws-atlas" {
   aws_account_id         = data.aws_caller_identity.account.account_id
 }
 
+data "aws_route_tables" "private_rt_filters" {
+  vpc_id = var.vpc.vpc_id
+  filter  {
+    name = "tag:Name"
+    values = [
+       "${var.vpc.name}-private"
+    ]
+  }
+}
+
+data "aws_route_tables" "public_rt_filters" {
+  vpc_id = var.vpc.vpc_id
+  filter  {
+    name = "tag:Name"
+    values = [
+       "${var.vpc.name}-public"
+    ]
+  }
+}
+
 resource "aws_route" "peeraccess" {
   route_table_id            = data.aws_vpc.primary.main_route_table_id
   destination_cidr_block    = var.atlas_vpc_cidr
   vpc_peering_connection_id = mongodbatlas_network_peering.aws-atlas.connection_id
   depends_on                = [aws_vpc_peering_connection_accepter.peer]
+}
+
+resource "aws_route" "peerprivatertaccess" {
+  count                     = length(data.aws_route_tables.private_rt_filters.ids)
+  route_table_id            = tolist(data.aws_route_tables.private_rt_filters.ids)[count.index]
+  destination_cidr_block    = var.atlas_vpc_cidr
+  vpc_peering_connection_id = mongodbatlas_network_peering.aws-atlas.connection_id
+  depends_on                = [aws_vpc_peering_connection_accepter.peer]
+}
+
+resource "aws_route" "peerpublicrtaccess" {
+  count                     = length(data.aws_route_tables.public_rt_filters.ids)
+  route_table_id            = tolist(data.aws_route_tables.public_rt_filters.ids)[count.index]
+  destination_cidr_block    = var.atlas_vpc_cidr
+  vpc_peering_connection_id = mongodbatlas_network_peering.aws-atlas.connection_id
+  depends_on                = [aws_vpc_peering_connection_accepter.peer]
+}
+
+##IP Access List entry resource. The access list grants access from IPs, CIDRs or AWS Security Groups (if VPC Peering is enabled) to clusters within the Project.
+resource "mongodbatlas_project_ip_access_list" "atlas-ip-access-list-1" {
+  project_id = var.atlas_project_id
+  cidr_block = data.aws_vpc.primary.cidr_block
+  comment    = "CIDR block of the VPC"
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
