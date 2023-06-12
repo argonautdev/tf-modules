ac651b137b21b0a88e14485d495147d1777fe94f
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2023-06-12 06:55:10 +0000

    Provider Version

--- modules/aws/s3Bucket/provider.tf
+++ modules/aws/s3Bucket/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.9"
+      version = "~> 4.9"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
   }
 }
