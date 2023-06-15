0987d136dbbdbbc5c931fa3f7eedf4e0ece89932
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2023-06-15 02:40:08 +0000

    Elasticache, EKS, Redis provider block fix

--- modules/aws/elasticacheRedis/provider.tf
+++ modules/aws/elasticacheRedis/provider.tf
@@ -1,9 +1,13 @@
 terraform {
   required_version = ">= 0.15"
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
+    }
     null = {
       source  = "hashicorp/null"
-      version = ">= 2.0"
+      version = "~> 2.1.2"
     }
   }
 }
