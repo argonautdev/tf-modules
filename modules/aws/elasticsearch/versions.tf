5ada14c35ca93d4aa2b2c03acd0c1e82d279904a
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2023-06-16 03:08:30 +0000

    Elasticsearch, elasticache module provider fix

--- modules/aws/elasticsearch/versions.tf
+++ modules/aws/elasticsearch/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = "~> 4.67.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 2.0"
+      version = "~> 2.1.2"
     }
   }
 }
