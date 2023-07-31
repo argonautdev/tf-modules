19d82d753ce1150a0749a6fc94fb14b8068c0512
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2023-07-31 12:02:55 +0000

    EKS Upstream Module version change

--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -50,7 +50,7 @@ data "aws_subnets" "private_secondary" {
 }
 
 module "eks" {
-  source = "github.com/argonautdev/terraform-eks?ref=v17.4.2"
+  source = "github.com/argonautdev/terraform-eks?ref=v17.4.3"
   cluster_name             = var.cluster.name
   cluster_version          = var.cluster.version
   wait_for_cluster_timeout = 900
